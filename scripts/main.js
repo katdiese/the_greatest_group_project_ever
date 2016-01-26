5c3bf12789413fb264446933007a2fe0db360dc5
Caleb Pollman <cpollman1@gmail.com>
2016-01-26 13:07:51 -0700

    completed merge conflict

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,15 @@ $(document).ready (function () {
 		//autopopulate billing values
 	})
 })
+
+$(document).ready(function() {
+  $.ajax({
+    url: 'data/inventory.json',
+    method: 'GET'
+	  }).then(function(data) {
+	$('#item1').html('<img src=' + data.sloths[0].image + '>' + '<figcaption>' + data.sloths[0].name + "<br>$" + data.sloths[1].price + "</figcaption>");
+    $('#item2').html('<img src=' + data.sloths[3].image + '>' + '<figcaption>' + data.sloths[3].name + "<br>$" + data.sloths[3].price + "</figcaption>");
+    $('#item3').html('<img src=' + data.sloths[2].image + '>' + '<figcaption>' + data.sloths[2].name + "<br>$" + data.sloths[2].price + "</figcaption>");
+   });
+ 
+ });
