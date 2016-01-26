f1a4ffdc7810a5b343b6c503e7a2fcdd9cb22fad
Kathy Dieser <katdiese@gmail.com>
2016-01-26 10:50:01 -0700

    appended images to dom, styled sizes

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
   $.ajax({
-    url: 'http://127.0.0.1:8080/data/inventory.json]]',
+    url: 'data/inventory.json',
     method: 'GET'
   }).then(function(data) {
-    console.log(data);
+    $('#item1').html('<img src=' + data.sloths[0].image + '>' + '<figcaption>' + data.sloths[0].name + "<br>$" + data.sloths[1].price + "</figcaption>");
+    $('#item2').html('<img src=' + data.sloths[3].image + '>' + '<figcaption>' + data.sloths[3].name + "<br>$" + data.sloths[3].price + "</figcaption>");
+    $('#item3').html('<img src=' + data.sloths[2].image + '>' + '<figcaption>' + data.sloths[2].name + "<br>$" + data.sloths[2].price + "</figcaption>");
   });
 
 });
