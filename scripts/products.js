761a1be091adfa326e1bcb43899cb48567f0954c
Kathy Dieser <katdiese@gmail.com>
2016-01-26 13:08:40 -0700

    JS to add all the products to the products page dynamically

--- scripts/products.js
+++ scripts/products.js
@@ -3,25 +3,31 @@ $(document).ready(function() {
     url: 'data/inventory.json',
     method: 'GET'
   }).then(function(data) {
+    // Find each animal type in the date using Object.keys
     var obj = Object.keys(data);
-    // console.log(obj)
+    // Loop through each animal type
     for(i = 0; i < obj.length; i++) {
       var species = obj[i];
-      console.log(obj[i]);
+      // Isolate the array of data for each animal type
       var speciesArray = data[species];
+      // Loop through each specific animal and append their data to the DOM
       for(j = 0; j < speciesArray.length; j++) {
         var eachAnimal = speciesArray[j];
-        console.log(speciesArray[j].name);
-        $('body').append('<div><img class="img-responsive" src=' + eachAnimal.image + "></div>")
+        var animalName = eachAnimal.name;
+        var animalAge = eachAnimal.age;
+        var animalGender = eachAnimal.gender;
+        var animalPrice = "$"+eachAnimal.price;
+        var animalDescription = "This " +animalGender + " animal is " + animalAge + " years old" ;
+        var animalImage = eachAnimal.image;
+
+        var animalBlock = '<div class="row"><image class="col-md-4"src="'+ animalImage +'"></image><div class="col-md-8"><h2>'+ animalName +'</h2><p>'+ animalPrice +'</p><p>'+ animalDescription +'</p></div></div>';
+
+        var createAnimalBlock =function () {
+          $('#products').append(animalBlock)
+        }
+
+        createAnimalBlock();
       }
     }
-    // console.log(Object.keys(data));
   });
 });
-
-//id=products
-
-var createBlock = function(data) {
-  var species = Object.keys(data);
-  console.log(species);
-}
