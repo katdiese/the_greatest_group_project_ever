583440f707e7a6b276645f1f97d865be794995f8
Dennis Bailey <bailey.dennis@gmail.com>
2016-01-27 16:09:24 -0700

    Added a hover event to the three images

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,35 @@
 //clicking checkbox on checkout.html disables billing text fields - CP
-$(document).ready (function () {
-	$('#sameAsBilling').change(function () {
-		$('#firstNameBilling').prop('disabled', $(this).is(':checked'));
-		$('#lastNameBilling').prop('disabled', $(this).is(':checked'));
-		$('#emailBilling').prop('disabled', $(this).is(':checked'));
-		$('#addressLineOneBilling').prop('disabled', $(this).is(':checked'));
-		$('#addressLineTwoBilling').prop('disabled', $(this).is(':checked'));
-		$('#stateBilling').prop('disabled', $(this).is(':checked'));
-		$('#zipCodeBilling').prop('disabled', $(this).is(':checked'));
-	});
-});
-
-//Customer Object - CP
-var Customer = function () {
-	this.firstNameShipping = firstNameShipping;
-	this.lastNameShipping = lastNameShipping;
-	this.emailShipping = emailShipping;
-	this.addressLineOneShipping = addressLineOneShipping;
-	this.addressLineTwoShipping = addressLineTwoShipping;
-	this.stateShipping = stateShipping;
-	this.zipCodeShipping = zipCodeShipping;
-	this.firstNameBilling = firstNameBilling;
-	this.emailBilling = emailBilling;
-	this.addressLineOneBilling = addressLineOneBilling;
-	this.addressLineTwoBilling = addressLineTwoBilling;
-	this.stateBilling = stateBilling;
-	this.zipCodeBilling = zipCodeBilling;
-	this.creditCardNumber = creditCardNumber;
-	this.expirationDate = expirationDate;
-	this.ccvNumber = ccvNumber;
-};
+// $(document).ready (function () {
+// 	$('#sameAsBilling').change(function () {
+// 		$('#firstNameBilling').prop('disabled', $(this).is(':checked'));
+// 		$('#lastNameBilling').prop('disabled', $(this).is(':checked'));
+// 		$('#emailBilling').prop('disabled', $(this).is(':checked'));
+// 		$('#addressLineOneBilling').prop('disabled', $(this).is(':checked'));
+// 		$('#addressLineTwoBilling').prop('disabled', $(this).is(':checked'));
+// 		$('#stateBilling').prop('disabled', $(this).is(':checked'));
+// 		$('#zipCodeBilling').prop('disabled', $(this).is(':checked'));
+// 	});
+// });
+// 
+// //Customer Object - CP
+// var Customer = function () {
+// 	this.firstNameShipping = firstNameShipping;
+// 	this.lastNameShipping = lastNameShipping;
+// 	this.emailShipping = emailShipping;
+// 	this.addressLineOneShipping = addressLineOneShipping;
+// 	this.addressLineTwoShipping = addressLineTwoShipping;
+// 	this.stateShipping = stateShipping;
+// 	this.zipCodeShipping = zipCodeShipping;
+// 	this.firstNameBilling = firstNameBilling;
+// 	this.emailBilling = emailBilling;
+// 	this.addressLineOneBilling = addressLineOneBilling;
+// 	this.addressLineTwoBilling = addressLineTwoBilling;
+// 	this.stateBilling = stateBilling;
+// 	this.zipCodeBilling = zipCodeBilling;
+// 	this.creditCardNumber = creditCardNumber;
+// 	this.expirationDate = expirationDate;
+// 	this.ccvNumber = ccvNumber;
+// };
 
 
 //appends images of featured animals to index.html - KD
@@ -43,5 +43,25 @@ $(document).ready(function() {
     $('#item2').html('<img class="img-responsive center-block" src=' + data.sloths[3].image + '>' + '<figcaption>' + data.sloths[3].name + "<br>$" + data.sloths[3].price + "</figcaption>");
     $('#item3').html('<img class="img-responsive center-block" src=' + data.sloths[2].image + '>' + '<figcaption>' + data.sloths[2].name + "<br>$" + data.sloths[2].price + "</figcaption>");
    });
+   
+// Carousel
+
+
+
+// Hover actions
+
+$(document).on('mouseenter', 'img', function() {
+  $(this).css('opacity', .5);
+})
+
+$(document).on('mouseleave', 'img', function() {
+  $(this).css('opacity', 1);
+})   
 
 });
+
+
+
+
+
+
